af835ec2ab73b292d92b701fbee4a028f0c73bdf
real-ara-w <radityawigunanta77@gmail.com>
2025-02-20 12:03:06 +0700

    retrieve users

--- app/Filament/Resources/ImageResource/Pages/ListImages.php
+++ app/Filament/Resources/ImageResource/Pages/ListImages.php
@@ -9,11 +9,4 @@ use Filament\Resources\Pages\ListRecords;
 class ListImages extends ListRecords
 {
     protected static string $resource = ImageResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
